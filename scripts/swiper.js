7331d734f67017ef162acf2d4c51e41d49451525
no-okane <dnjsgml3304.15@gmail.com>
2025-07-29 15:03:22 +0900

    2025-07-29

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -17,3 +17,24 @@ const media = new Swiper ('#media',{
 })
 
 console.log('Next 버튼:', document.querySelector('#top1 .swiper-button-prev'));
+
+
+const edu = new Swiper ('#edu',{
+    loop:true,
+    autoplay:{
+        delay:3500,
+        disableOnInteraction: false,
+    },
+    navigation:{
+        nextEl:'#edu .swiper-button-next',
+        prevEl:'#edu .swiper-button-prev',
+    },
+    scrollbar:{
+        el:'#edu ~ .swiper-controls .swiper-scrollbar'
+    },
+    pagination: {
+        el: "#edu ~ .swiper-controls .pagination_deco .swiper-pagination",
+        type: "fraction",
+        clickable: true,
+    },
+})
